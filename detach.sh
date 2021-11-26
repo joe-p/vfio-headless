ab04398845796ed28c44441cbb8dffafb7d48e91
Joe Polny <joepolny@gmail.com>
2021-11-25 21:39:17 -0500

    manually detach pci devices

---
+++ detach.sh
@@ -0,0 +1,7 @@
+virsh nodedev-detach pci_0000_0b_00_0
+virsh nodedev-detach pci_0000_0b_00_1
+virsh nodedev-detach pci_0000_07_00_0
+virsh nodedev-detach pci_0000_04_00_0
+virsh nodedev-detach pci_0000_0e_00_3
+virsh nodedev-detach pci_0000_09_00_0
+virsh nodedev-detach pci_0000_01_00_0
